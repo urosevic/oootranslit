8398a47882524543bcfb6f7da38e54f515dddee5
Aleksandar Urosevic <urke.kg@gmail.com>
2024-02-03 20:17:45 +0100

    Initiate OOoTranslit Git repo

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Set the path to the source directory.
+source_dir="source"
+
+# Set the path to the dist directory.
+dist_dir="dist"
+
+# Check if the 'dist' directory exists, and create it if not.
+if [ ! -d "$dist_dir" ]; then
+  mkdir -p "$dist_dir"
+fi
+
+# Change to the source directory.
+cd "$source_dir"
+
+# Get the version value from the description.xml file.
+version=$(grep -oP '(?<=<version value=")[^"]+' description.xml)
+
+# Set the ZIP filename without path.
+zip_filename="OOoTranslit-$version.oxt"
+
+# Zip all files in the source directory to a file named OOoTranslit-VERSION.oxt
+zip -9 -Z bzip2 -X -o -r "../$dist_dir/$zip_filename" . && \
+echo "--> Zip file created: $zip_filename" && \
+cd "../$dist_dir" && \
+# Create an MD5 hash file for the compressed zip archive
+md5sum "$zip_filename" > "$zip_filename.md5" && \
+echo "--> MD5 hash file created: $zip_filename.md5"
---
+++ source/readme-sr_RS.txt
@@ -0,0 +1,17 @@
+OOoTranslit 24.02.0
+
+Izdanje 24.02.0 unapređuje preslovljavanje iz latinice u ćirilicu.
+Promenjen je naziv novinarskog rasporeda u Tanjug.
+
+Ukoliko uočite greške u radu ovog dodatka, molim Vas da ih prijavite
+na forumu "Otvoreni dokument" na adresi http://otvorenidokument.com
+ili preko formulara za kontakt na adresi https://urosevic.net/kontakt
+
+Saznajte više o dodatku na:
+https://urosevic.net/oootranslit
+
+Zvanična Facebook stranica dodatka:
+https://www.facebook.com/oootranslit
+
+Autor proširenja je Aleksandar Urošević, TechWebUX doo Kragujevac.
+Koautor je Goran Rakić.
